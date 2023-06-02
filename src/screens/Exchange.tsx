fb5092aa679e868603a399c40e20bc6e4c1b2421
raulglezrdguez <raulglezrdguez69@gmail.com>
2023-06-02 11:54:06 -0400

    screen Exchange created

---
+++ src/screens/Exchange.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Exchange = () => {
+  return (
+    <View>
+      <Text>Exchange</Text>
+    </View>
+  );
+};
+
+export default Exchange;
+
+const styles = StyleSheet.create({});
