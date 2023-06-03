56abb371461288e779806e8594ed1cd9e8d8d34a
raulglezrdguez <raulglezrdguez69@gmail.com>
2023-06-03 11:00:33 -0400

    App updated with scrollview

--- App.tsx
+++ App.tsx
@@ -20,9 +20,11 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
   },
   scroll: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     alignContent: 'center',
